b4f85613eb5ec9804cd37c6e3fe75a6564450de2
Marina Marenkov <marina.marenkov@gmail.com>
2021-01-06 12:53:03 +0200

    123

--- vars.tf
+++ vars.tf
@@ -1,6 +1,9 @@
-variable region { default = "eu-frankfurt-1" }
-variable compartment_ocid { default = "" }
-variable compute_images {
+variable "region" { 
+    default = "eu-frankfurt-1"
+    description = "region" 
+}
+variable "compartment_ocid" { default = "" }
+variable "compute_images" {
     type = list
     default = ["minikube","dev-linux"]
 }
