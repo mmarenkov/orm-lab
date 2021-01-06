0ed1701983e02893737356bec57748a01e898e95
Marina Marenkov <marina.marenkov@gmail.com>
2021-01-06 12:26:35 +0200

    06012021

--- srcfiles/outputs.tf
+++
@@ -1,6 +0,0 @@
-# Outputs for the vcn module
-
-output "vcn_id" {
-  description = "OCID of the VCN that is created"
-  value = oci_core_vcn.tf-demo-prod.id
-}
--- srcfiles/vars.tf
+++
@@ -1,5 +0,0 @@
-variable export_mm-dev-vm_source_image_id { default = "ocid1.image.oc1..aaaaaaaa2es7kqqgmmjyymzdaaeqmmehrprg6gdjxs4on5lpzwiv64przksa" }
-variable region { default = "eu-frankfurt-1" }
-variable compartment_ocid { default = "" }
-variable export_minikube_source_image_id { default = "ocid1.compartment.oc1..aaaaaaaafdcbpj4a7n26szmo5onpubjnuizaone4w3rdrrcxe3lrsk2e63kq" }
-variable export_mm-dev-linux_source_image_id { default = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaq7xbqvz7ydv7qgyr45sk3ydf7x4xq2ov2nffzauy4gzqmcjzdxna" }
